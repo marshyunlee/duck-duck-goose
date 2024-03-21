bdb591b0aee8d6ba369581b88f58bdf991825b19
Marshall <gkslql456@gmail.com>
2024-03-21 16:58:12 -0700

    Update scene.js

--- script/scene.js
+++ script/scene.js
@@ -56,8 +56,8 @@ const initScene = async () => {
 	controls.minPolarAngle = Math.PI/2 - 0.5;
 	controls.maxPolarAngle = Math.PI/2 - 0.5;
 	controls.zoomSpeed = 0.3;
-	controls.minZoom = 200;
-	controls.maxZoon = 500;
+	controls.minDistance = 100;
+	controls.maxDistance = 300;
 	controls.enableDamping = true;
 	controls.dampingFactor = 0.3;
 	
